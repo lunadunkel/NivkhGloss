783a399e3be33c64cb9a5c15c160986ae59e185b
lunadunkel <125760208+lunadunkel@users.noreply.github.com>
2025-04-22 01:54:54 +0300

    Update BasicNeuralClassifier.py

--- classes/BasicNeuralClassifier.py
+++ classes/BasicNeuralClassifier.py
@@ -2,30 +2,39 @@ import torch
 import numpy as np
 import torch.nn as nn
 from sklearn.metrics import accuracy_score
+from TorchCRF import CRF
 
 class BasicNeuralClassifier(nn.Module):
-    def __init__(self, vocab_size, labels_number, device="cpu", criterion=nn.NLLLoss(reduction="mean"), **kwargs):
+    """ Базовый классификатор
+    включает возможность:
+    - CRF-слой
+    - BPE-слой
+    
+    def __init__(self, vocab_size, labels_number, bpe_vocab_size=None, device="cpu", criterion=nn.NLLLoss(reduction="mean"), **kwargs):
         super().__init__()
         self.vocab_size = vocab_size
         self.labels_number = labels_number
+        self.bpe_vocab_size = bpe_vocab_size
         self.device = device
-        self.build_network(vocab_size, labels_number, **kwargs)
+        self.build_network(vocab_size, labels_number, bpe_vocab_size=bpe_vocab_size, **kwargs)
         self.criterion = criterion
 
-    def build_network(self, vocab_size, labels_number, **kwargs):
+    def build_network(self, vocab_size, labels_number, bpe_vocab_size=None, **kwargs):
         raise NotImplementedError("You should implement network construction in your derived class.")
 
-    def forward(self, inputs, mask=None):
+    def forward(self, input_ids, bpe_boundary_labels=None, mask=None):
         raise NotImplementedError("You should implement forward pass in your derived class.")
 
-    def predict(self, input_ids, mask=None):
+    def predict(self, input_ids, bpe_boundary_labels=None, mask=None):
         self.eval()
         with torch.no_grad():
             input_ids = input_ids.to(self.device)
+            bpe_boundary_labels = bpe_boundary_labels.to(self.device) if bpe_boundary_labels is not None else None
             mask = self._prepare_mask(input_ids, mask)
-            outputs = self(input_ids, mask=mask)
+            outputs = self(input_ids, bpe_boundary_labels=bpe_boundary_labels, mask=mask)
 
             if self.use_crf: # использование CRF
+                
                 return self.crf.viterbi_decode(outputs["logits"], mask)
             else:
                 preds = torch.argmax(outputs["log_probs"], dim=-1).cpu().tolist()
@@ -33,16 +42,15 @@ class BasicNeuralClassifier(nn.Module):
 
     def _prepare_mask(self, input_ids, mask):
         if mask is None:
-            mask = (input_ids != 0)
+            mask = (input_ids != 0)  # Маска: True для ненулевых элементов
         return mask.to(self.device).bool()
-
 
     def calculate_accuracy(self, labels, predictions, mask):
         true_labels = []
         true_preds = []
         for preds, lbls, m in zip(predictions, labels, mask):
             if m is None:
-                active = [True] * len(lbls) 
+                active = [True] * len(lbls)
             elif isinstance(m, torch.Tensor):
                 active = m.bool().tolist()  # тензор в булевый формат
             elif isinstance(m, list):
