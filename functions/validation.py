ebbca69a423d2c0cf614b7bbc3f7da41967cf941
lunadunkel <125760208+lunadunkel@users.noreply.github.com>
2025-04-22 15:11:11 +0300

    Update validation.py

--- functions/validation.py
+++ functions/validation.py
@@ -38,10 +38,12 @@ def validate_model(model, data_loader, device, use_bpe=False):
                 mask = [m[:torch.sum(m).item()].tolist() for m in mask.cpu()]
             else:
                 preds = torch.argmax(outputs['log_probs'], dim=-1).cpu().tolist()
+                labels = labels.cpu().tolist()
+                mask = mask.cpu().tolist()
 
             all_preds.extend(preds)
             all_labels.extend(labels.cpu().tolist())
-            all_masks.extend(mask.cpu().tolist())
+            all_masks.extend(mask)
     accuracy = model.calculate_accuracy(all_labels, all_preds, all_masks)
     word_level_accuracy = model.calculate_word_level_accuracy(all_labels, all_preds)
     avg_loss = total_loss / len(data_loader)
