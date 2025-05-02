49a2a8dae1da644b8b80cb6c0959f39acfbad5f0
lunadunkel <125760208+lunadunkel@users.noreply.github.com>
2025-05-02 18:01:06 +0300

    corrections
    Signed-off-by: lunadunkel <125760208+lunadunkel@users.noreply.github.com>

--- functions/glossing/glossing_dataset.py
+++ functions/glossing/glossing_dataset.py
@@ -4,18 +4,22 @@ def create_glossing_dataset(data):
     final_data = []
     for sent in data:
         segm = re.sub('\ufeff', '', sent['segmented'])
-        segm = re.sub('^\t', '', segm)  
-        gloss = re.sub('^\t', '', sent['glossed'])  
+        segm = re.sub('^\t', '', segm)
+        segm = re.sub('[\,\?\!\;«\']+', '\t', segm)
+        gloss = re.sub('[\,\?\!\;«\']+', '\t', sent['glossed'])
+        gloss = re.sub('^\t', '', gloss)
 
         gold_segm = re.sub('\t', '\t<SEP>\t', segm)
         gold_gloss = re.sub('\t', '\t<SEP>\t', gloss)
 
         gold_segm = re.sub('-', '\t<HYPH>\t', gold_segm)
         gold_gloss = re.sub('-', '\t<HYPH>\t', gold_gloss)
-        
-        gold_gloss = re.sub('[А-яё]+', '<STEM>', gold_gloss)
-        dictionary = {'segmented': gold_segm,
-                      'STEM_glossed': gold_gloss,
+
+        gold_gloss = re.sub('(?:[А-яё\.]+[А-яё])(?!\.CAUSEE|\.DU|\.EXCL|\.INCL)', '<STEM>', gold_gloss)
+        gold_gloss = re.sub('(?:<STEM>\.)+<STEM>', '<STEM>', gold_gloss)
+        gold_gloss = re.sub('(?<=[A-z])\.(?=(?:$| |\t))', '', gold_gloss)
+        dictionary = {'segmented': gold_segm.split('\t'),
+                      'STEM_glossed': gold_gloss.split('\t'),
                       'id': sent['id'],
                       'sents': sent['sents']}
         final_data.append(dictionary)
