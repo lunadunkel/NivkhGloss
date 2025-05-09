7207de0fc4d58978b91548ac869b17805c3cad42
lunadunkel <savinadaria1804@gmail.com>
2025-05-09 20:15:59 +0300

    Update pos_dataset.py

--- models/pos_tagging/data_loader/pos_dataset.py
+++ models/pos_tagging/data_loader/pos_dataset.py
@@ -5,9 +5,12 @@ def prepare_sequence(seq, to_ix):
     return torch.tensor([to_ix[word] if word in to_ix else to_ix['<UNK>'] for word in seq], dtype=torch.long)
 
 class PosDataset(Dataset):
-    def __init__(self, sentences, device='cpu'):
+    def __init__(self, sentences, word_dict, label_dict, char_dict, device='cpu'):
         self.sentences = sentences
         self.device = device
+        self.word_dict = word_dict
+        self.label_dict = label_dict
+        self.char_dict = char_dict
 
     def __len__(self):
         return len(self.sentences)
@@ -15,9 +18,9 @@ class PosDataset(Dataset):
     def __getitem__(self, idx):
         sentence = self.sentences[idx]
         answer = dict()
-        answer['input_ids'] = prepare_sequence(sentence['input'].split('\t'), WORD_TO_IDX).to(self.device)
-        answer['char_ids'] = prepare_sequence(list(sentence['input'].replace('\t', '#')), char_dict).to(self.device)
-        answer['labels'] = prepare_sequence(sentence['label'].split('\t'), TAG_TO_IDX).to(self.device)
+        answer['input_ids'] = prepare_sequence(sentence['input'].split('\t'), self.word_dict).to(self.device)
+        answer['char_ids'] = prepare_sequence(list(sentence['input'].replace('\t', '#')), self.char_dict).to(self.device)
+        answer['labels'] = prepare_sequence(sentence['label'].split('\t'), self.char_dict).to(self.device)
         answer['mask'] = answer['input_ids'] != 0
         answer['index']
         return answer
